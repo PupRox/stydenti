ea29c47474a9f363c61933980b9a6c8464fc3526
PupRox <roxphl2@gmail.com>
2023-11-08 19:51:30 +0300

    Реализованна функция редактирования

--- stydenti/Source.cpp
+++ stydenti/Source.cpp
@@ -1,128 +1,103 @@
 #include <iostream>
 #include <string>
+#include "Person.h"
+#include "Student.h"
 using namespace std;
 
-class Person
-{
-private:
-	int age;
-	string name;
-
-public:
-	Person(int age, string name) {
-		this->age = age;
-		this->name = name;
-	}
-	Person() {
-		age = 0;
-		name = " ";
-	}
-	void print_per() {
-		cout << "Name -> " << name << "\tAge -> " << age;
-	}
-	string write_per() {
-		cout << "Write Name->\t";
-		cin >> name;
-		cout << "Write Age->\t";
-		cin >> age;
-		return name; age;
-	}
-
-
-};
-class Student: public Person {
-public: 
-	int id;
-	string kyrs;
-	string groop;
-	void print_stud() {
-		cout << "\tID -> " << id << "\tKyrs -> " << kyrs << "\tGroop -> "<<groop<<"\t";
-	}
-	int write_id() {
-		cout << "Write ID->\t";
-		cin >> id;
-		return id; 
-	}
-	string wtite_kyrs() {
-		cout << "Write Kyrs->\t";
-		cin >> kyrs;
-		return kyrs;
-	}
-	string write_groop() {
-		cout << "Write Groop->\t";
-		cin >> groop;
-		return groop;
-	}
-
-};
 
 int main() {
 	int rows;
-	int colums = 1;
-	int z,x;
+	int z, x, o;
 	bool stop = true;
 	cout << "Write numbers of stydent" << endl;
 	cin >> rows;
-	Student**mass = new Student*[rows];
-	for (int i = 0; i < rows; i++)
-	{
-		mass[i] = new Student[colums];
-	}
-	
+	Student* mass = new Student[rows];
+
 	while (stop)
 	{
-		cout << "1. Creat new Stydent" << endl << "2. Read base of stydent" << endl << "3. Change information of stydent" << endl;
-		cout << "4. Remove stydent" << endl << "0. Exit" << endl;
+		cout <<endl<< "1. Creat new Stydent" << endl << "2. Read base of stydent" << endl << "3. Change information of stydent" << endl;
+		cout << "4. Remove stydent" << endl << "9. Exit" << endl;
 		cin >> z;
 		switch (z)
 		{
+			//Заполнение массива
 		case 1: for (int i = 0; i < rows; i++)
 		{
 			cout << endl;
-			for (int j = 0; j < colums; j++)
-			{
-				mass[i][j].write_per();
-				mass[i][j].write_id();
-				mass[i][j].wtite_kyrs();
-				mass[i][j].write_groop();
-			}	
+
+			mass[i].write_name();
+			mass[i].write_age();
+			mass[i].write_id();
+			mass[i].wtite_kyrs();
+			mass[i].write_groop();
+
 		}
 			  break;
+			  //Вывод инфор в массивах
 		case 2: for (int i = 0; i < rows; i++)
 		{
 			cout << i << "->";
-			for (int j = 0; j < colums; j++)
-			{
-				mass[i][j].print_per();
-				mass[i][j].print_stud();
-			}
+			mass[i].print_id();
+			mass[i].print_name();
+			mass[i].print_age();
+			mass[i].print_kyrs();
+			mass[i].print_groop();
 			cout << endl;
 		}
 			  break;
-		/*case 3: {
-			cout << "Choose witch student you want change" << endl;
-			cin >> x;
-			switch (x)
+		case 3:
+			for (int i = 0; i < rows; i++)
 			{
-			case 1: 
+				cout << i << "->\t";
+				mass[i].print_id();
+				mass[i].print_name();
+				mass[i].print_age();
+				mass[i].print_kyrs();
+				mass[i].print_groop();
+				cout << endl;
 			}
-		}*/
-		/*case 4: {
+			cout << endl << "Choose witch student you want change" << endl;
+			cin >> x;
+				cout << "0. ID";
+				cout << "\t1. Name";
+				cout << "\t2. Age";
+				cout << "\t3. Kyrs";
+				cout << "\t4. Groop";
+				cout << endl;
+				cout << endl << "What u want change?" << endl;
+				cin >> o;
+				for (int i = 0; i < rows; i++)
+				{
+					if (x==i)
+					{
+						switch (o)
+						{
+						case 0: mass[x].write_id(); break;
+						case 1: mass[x].write_name(); break;
+						case 2: mass[x].write_age(); break;
+						case 3: mass[x].wtite_kyrs(); break;
+						case 4: mass[x].write_groop(); break;
+						}
+					}
+				}
+				break;
+			
+			
+			
+			/*case 4: {
 			cout << "Choose witch student you want remove" << endl;
 			cin >> x;
 
-		}*/
-		case 0: stop = false; break;
+			}*/
+		case 9: stop = false;
+			delete[] mass;
+			break;
 		}
-		
-	}
-	for (int i = 0; i < rows; i++)
-	{
-		delete[] mass[i];
-	}
-	delete[] mass;
-	
 
-	
+	}
+
+
+
+
 	return 0;
 }
